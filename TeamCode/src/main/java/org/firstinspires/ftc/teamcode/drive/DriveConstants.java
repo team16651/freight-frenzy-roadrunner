688b0cd96fca3fa0462575c3146dff48aa91084a
dustingood <AbigailNevaehMason>
2021-11-29 20:51:13 -0600

    - Enable JUnit - Odometry Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15; // in
+    public static double TRACK_WIDTH = 9.72; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01765;
+    public static double kV = 0.01685;
     public static double kA = 0.0033;
-    public static double kStatic = 0.0022;
+    public static double kStatic = 0.0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
---
+++ TeamCode/src/test/org/firstinspires/ftc/teamcode/hardware/ArmTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArmTest {
+
+    @Test
+    public void togglePosition() {
+
+    }
+}
